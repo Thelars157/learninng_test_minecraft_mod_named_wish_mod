2c1e67d2353e96a1840856253ce5c08951bdc015
Thelars157 <147783091+Thelars157@users.noreply.github.com>
2024-06-23 03:37:49 -0400

    Update wish_mod.java

--- forge/src/main/java/net/Thelars/wish_mod/wish_mod.java
+++ forge/src/main/java/net/Thelars/wish_mod/wish_mod.java
@@ -17,7 +17,6 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(wish_mod.MOD_ID)
 public class wish_mod {
         public static final String MOD_ID = "wish_mod";
@@ -30,9 +29,6 @@ public class wish_mod {
 
         ModItems.register(modEventBus);
         ModBlocks.register(modEventBus);
-
-
-
 
         modEventBus.addListener(this::commonSetup);
        
@@ -53,13 +49,11 @@ public class wish_mod {
     }
 
 
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
     public void onServerStarting(ServerStartingEvent event)    {
     
     }
     
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
     @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ClientModEvents    {
         @SubscribeEvent
